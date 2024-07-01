9b531f07ca822811563007be89a8dd1b622d5299
David <nevermore6264@example.com>
2024-07-02 03:33:10 +0700

    first commit

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -6,18 +6,10 @@ interface AppLayoutProps {
 }
 
 export const metadata: Metadata = {
-    title: 'PrimeReact Sakai',
+    title: 'Code Star',
     description: 'The ultimate collection of design-agnostic, flexible and accessible React UI Components.',
     robots: { index: false, follow: false },
     viewport: { initialScale: 1, width: 'device-width' },
-    openGraph: {
-        type: 'website',
-        title: 'PrimeReact SAKAI-REACT',
-        url: 'https://sakai.primereact.org/',
-        description: 'The ultimate collection of design-agnostic, flexible and accessible React UI Components.',
-        images: ['https://www.primefaces.org/static/social/sakai-react.png'],
-        ttl: 604800
-    },
     icons: {
         icon: '/favicon.ico'
     }
