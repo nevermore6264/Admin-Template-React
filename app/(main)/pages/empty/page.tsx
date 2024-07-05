a7ff7169ecd8f763108ea4c6e1dc664ae6d22a41
David <spteam.coder@gmail.com>
2024-07-05 22:38:32 +0700

    commit

--- app/(main)/pages/empty/page.tsx
+++ app/(main)/pages/empty/page.tsx
@@ -1,6 +1,12 @@
-import React from 'react';
+"use client";
+
+import { Calendar } from 'primereact/calendar';
+import React, { useState } from 'react';
+import { Nullable } from 'primereact/ts-helpers';
 
 const EmptyPage = () => {
+    const [date, setDate] = useState<Nullable<Date>>(null);
+
     return (
         <div className="grid">
             <div className="col-12">
@@ -9,6 +15,8 @@ const EmptyPage = () => {
                     <p>Use this page to start from scratch and place your custom content.</p>
                 </div>
             </div>
+            <Calendar value={date} onChange={(e) => setDate(e.value)} />
+            <h2>{date?.toLocaleDateString() ?? 'xxx'}</h2>
         </div>
     );
 };
--- layout/AppTopbar.tsx
+++ layout/AppTopbar.tsx
@@ -1,16 +1,35 @@
 /* eslint-disable @next/next/no-img-element */
 
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import { classNames } from 'primereact/utils';
 import React, { forwardRef, useContext, useImperativeHandle, useRef } from 'react';
 import { AppTopbarRef } from '@/types';
 import { LayoutContext } from './context/layoutcontext';
+import { OverlayPanel } from 'primereact/overlaypanel';
+import { Menu } from 'primereact/menu';
 
 const AppTopbar = forwardRef<AppTopbarRef>((props, ref) => {
     const { layoutConfig, layoutState, onMenuToggle, showProfileSidebar } = useContext(LayoutContext);
-    const menubuttonRef = useRef(null);
-    const topbarmenuRef = useRef(null);
-    const topbarmenubuttonRef = useRef(null);
+    const menubuttonRef = useRef<HTMLButtonElement>(null);
+    const topbarmenuRef = useRef<HTMLDivElement>(null);
+    const topbarmenubuttonRef = useRef<HTMLButtonElement>(null);
+    const op = useRef<OverlayPanel>(null);
+    const router = useRouter();
+
+    const items = [
+        {
+            label: 'Settings',
+            icon: 'pi pi-cog'
+        },
+        {
+            label: 'Logout',
+            icon: 'pi pi-sign-out',
+            command: () => {
+                router.push('/auth/login');
+            }
+        }
+    ];
 
     useImperativeHandle(ref, () => ({
         menubutton: menubuttonRef.current,
@@ -38,12 +57,15 @@ const AppTopbar = forwardRef<AppTopbarRef>((props, ref) => {
                     <i className="pi pi-calendar"></i>
                     <span>Calendar</span>
                 </button>
-                <button type="button" className="p-link layout-topbar-button">
+                <button type="button" className="p-link layout-topbar-button" onClick={(e) => op.current?.toggle(e)}>
                     <i className="pi pi-user"></i>
                     <span>Profile</span>
                 </button>
+                <OverlayPanel ref={op}>
+                    <Menu model={items} style={{ border: 'none', boxShadow: 'none' }} />
+                </OverlayPanel>
             </div>
-        </div >
+        </div>
     );
 });
 
