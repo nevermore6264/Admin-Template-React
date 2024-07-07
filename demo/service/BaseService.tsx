e7c0f6451075249e9ca9ee0d87ac72451a066c19
David <nevermore6264@example.com>
2024-07-07 23:52:17 +0700

    Add

--- demo/service/BaseService.tsx
+++ demo/service/BaseService.tsx
@@ -1,25 +1,12 @@
 import axios, { AxiosResponse } from 'axios'; // Import axios and AxiosResponse for making HTTP requests
-import dotenv from 'dotenv'; // Import dotenv to load environment variables from .env file
-
-dotenv.config(); // Load environment variables from .env file
-
-// Define an interface for the configuration
-interface Config {
-    publicRuntimeConfig: {
-        url: string; // Base URL
-        path: string; // API path
-    };
-}
+// BaseService.tsx
+import getConfig from 'next/config';
 
 export class BaseService {
-    private baseUrl: string; // Base URL for service requests
+    private baseUrl: string;
 
     constructor(module: string) {
-        const config = this.getConfig(); // Get configuration from environment variables
-        if (!config.publicRuntimeConfig) {
-            throw new Error('Configuration is missing'); // Throw error if configuration is missing
-        }
-        this.baseUrl = `${config.publicRuntimeConfig.url}${config.publicRuntimeConfig.path}${module}`; // Construct base URL
+        this.baseUrl = getConfig().publicRuntimeConfig.url + getConfig().publicRuntimeConfig.path + module;
     }
 
     // Method to search with optional form data and event
@@ -59,23 +46,4 @@ export class BaseService {
         return await axios.get(`${this.baseUrl}/find-all`); // Get all entries
     }
 
-    // Private method to get configuration from environment variables
-    private getConfig(): Config {
-        // Log environment variables for debugging
-        console.log('API URL:', process.env.API_URL);
-        console.log('API PATH:', process.env.API_PATH);
-
-        // Ensure environment variables are defined
-        if (!process.env.API_URL || !process.env.API_PATH) {
-            throw new Error('API URL or PATH is not defined in the environment variables');
-        }
-
-        // Return configuration
-        return {
-            publicRuntimeConfig: {
-                url: process.env.API_URL!, // Use non-null assertion operator to tell TypeScript that these variables are not null
-                path: process.env.API_PATH!
-            }
-        };
-    }
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const dotenv = require('dotenv');
+dotenv.config();
 
-module.exports = nextConfig
+const nextConfig = {
+    reactStrictMode: false,
+
+    publicRuntimeConfig: {
+        // Các biến môi trường cần thiết
+        url: process.env.API_URL,
+        path: process.env.API_PATH
+    },
+    serverRuntimeConfig: {
+        // Will only be available on the server side
+    }
+};
+
+module.exports = nextConfig;
